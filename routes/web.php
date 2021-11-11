40defbe427ba4558c9f084dcf3bb6cb044b69009
rama11 <prof.agastyo@gmail.com>
2021-11-11 10:05:54 +0700

    Add route testing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/test', function () use ($router) {
+    return "Hi, this is testing";
+});
+
+
